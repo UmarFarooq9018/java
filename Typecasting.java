2efd7dc5d8b53112c46db96898dc99cd65f1b1d8
UmarFarooq9018 <90671157+UmarFarooq9018@users.noreply.github.com>
2022-07-28 13:25:57 +0530

    Create Typecasting.java

---
+++ Typecasting.java
@@ -0,0 +1,12 @@
+//Converting int to double
+class Main {
+  public static void main(String[] args) {
+    // create int type variable
+    int num = 10;
+    System.out.println("The integer value: " + num);
+
+    // convert into double type
+    double data = num;
+    System.out.println("The double value: " + data);
+  }
+}
